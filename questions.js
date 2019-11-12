aaaf0c626bc7cb84fdda6b84a7548cad49bbf12b
jhumbrac <john.humbracht@generalassemb.ly>
2019-11-11 20:45:19 -0600

    added questions.js questions for both html and js quiz

---
+++ questions.js
@@ -0,0 +1,55 @@
+var jsQuestions = [
+    {
+      title: "Which of the following is a disadvantage of using JavaScript?",
+      choices: ["Client-side JavaScript does not allow the reading or writing of files.", "JavaScript can not be used for Networking applications because there is no such support available.", "JavaScript doesn't have any multithreading or multiprocess capabilities.", "All of the above."],
+      answer: 'All of the above.'
+    },
+    {
+        title: 'You can pass a anonymous function as an argument to another function?',
+        choices: ['true', 'false'],
+        answer: 'true'
+    },
+    {
+        title: 'Which built-in method removes the last element from an array and returns that element?',
+        choices: ['last()', 'get()', 'pop()', 'None of the above.'],
+        answer: 'pop()'
+    },
+    {
+        title: 'Which built-in method reverses the order of the elements of an array?',
+        choices: [ 'changeOrder(order)', 'reverse()', 'sort(order)',  'None of the above.' ],
+        answer: 'reverse()'
+    },
+    {
+        title: 'Which of the following function of String object combines the text of two strings and returns a new string?',
+        choices: ['add()', 'merge()', 'concat()', 'append()'],
+        answer: 'concat()'
+    }
+];
+
+var htmlQuestions = [
+    {
+        title: 'Select the correct option to open a link in a new browser window?',
+        choices: [ 'A href="url" target="_blank"', 'A href="url" new', 'A href="url" target="new"', 'A href="url" target=_window"', 'None of the above.' ],
+        answer: 'A href="url" target="_blank"'
+    },
+    {
+        title: 'Select the option to make a list that lists the items with bullets?',
+        choices: ['List', 'UL', 'OL', 'DL', 'None of the above.' ],
+        answer: 'UL'
+    },
+    {
+        title: 'In CSS, what is the correct option to select all the p tags on a page?',
+        choices: [ '<p> { }', 'p { }', '#p { }', '.p { }', 'None of the above.' ],
+        answer: 'p { }'
+    },
+    {
+        title: 'Select the property that is used to create spacing between HTML elements?',
+        choices: [ 'border', 'spacing', 'margin', 'padding', 'None of the above.' ],
+        answer: 'margin'
+    },
+    {
+        title: 'In CSS,select the property used to set the background color of an image?',
+        choices: [ 'color', 'background:color', 'background-color', 'color:background'],
+        answer: 'background-color'
+    }
+];
