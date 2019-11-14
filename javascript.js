11d110ce698edcf15a3f4a7f572254be4b7bf3c5
jhumbrac <john.humbracht@generalassemb.ly>
2019-11-13 23:09:58 -0600

    Rebuilt js file to remove redundant code and get scope creep under control

--- javascript.js
+++ javascript.js
@@ -1,32 +1,28 @@
-// Declare variables on page
-
-
-// * The user arrives at the landing page and is presented with a call-to-action to "Start Quiz." Also note the navigation option to "View Highscores" and the "Time" value set at 0.
-// add highscores on page? date and time?
-var quizType = document.getElementById('quizType'); // select with js, html, css
 var startJs = document.getElementById('startJs');
 var startHtml = document.getElementById('startHtml');
-
-var question = document.getElementById('question');
-var form = document.querySelector('form');
-var formDiv = document.getElementById('formDiv');
-
 var answerBtn = document.getElementById('answerBtn');
 var finishedBtn = document.getElementById('finishedBtn');
 
+var question = document.getElementById('question');
+
+var formDiv = document.getElementById('formDiv');
+
+var initials = document.createElement('input');
+var initialsLabel = document.createElement('label');
+var initialsEntry;
+
 var introWrapper = document.getElementById('introWrapper');
 var jsWrapper = document.getElementById('jsWrapper');
-var htmlWrapper = document.getElementById('htmlWrapper');
 
-var highScores = document.getElementById('highScores');
+var highScoresDisplay = document.getElementById('highScores');
 var timeDisplay = document.getElementById('timeDisplay'); // display time remaining
 
+var answersElm = document.querySelector('input:checked + label');
 
+var highScores = [];
 var score = 0;
 var finalScore = 0;
 var j = 0;
-var k = 0;
-
 
 // initials.textContent = initialsEntry.value;
 // initials should be an object with key = initials, value = score. sorted high to low?
@@ -39,16 +35,16 @@ var timeRemaining = 60;
 function penalty() {
     timeRemaining -= 15;
     return timeRemaining;
-} 
+}
 function setTime() {
     clearInterval(interval);
     timeDisplay.textContent = timeRemaining;
-};
+}
 function stopTimer() {
     timeRemaining = '';
     setTime();
     renderTime();
-};
+}
 function renderTime() {
     timeDisplay.textContent = timeRemaining;
 }
@@ -59,8 +55,7 @@ function startTimer() {
         timeRemaining--;
         renderTime();
     }, 1000);
-};
-// * Clicking the "Start Quiz" button presents the user with a series of questions. The timer is initialized with a value and immediately begins countdown.
+}
 function removeContent() {
     formDiv.innerHTML = '';
 }
@@ -68,176 +63,126 @@ function postScores() {
     var scores = document.createElement('p');
     scores.textContent = `${localStorage.getItem(initialsEntry)} : ${localStorage.getItem(finalScore)}`;
     scores.setAttribute('class', 'scores');
-    highScores.append(scores);
+    highScoresDisplay.append(scores);
 }
 
-function startQuiz() {
-    introWrapper.classList = 'wrapper hidden'; // .add() need help with className vs classList!
-    jsWrapper.classList = 'wrapper';
+// function startQuiz() {
+//     toggleHidden(introWrapper, jsWrapper );
+
+//     // introWrapper.classList = 'wrapper hidden'; // .add() need help with className vs classList!
+//     // jsWrapper.classList = 'wrapper';
+// }
+// function startOver() {
+//     event.preventDefault();
+//     toggleHidden(jsWrapper, )
+//     jsWrapper.classList = 'wrapper hidden'; // .add() need help with className vs classList!
+//     introWrapper.classList = 'wrapper';
+//     // save local storage
+//     // check score against high scores
+//     // if highscore then would need to enter a separate panel
+//     // these comments should probably be a separate function?
+// }
+function toggleHidden(elem1, elem2) {
+    $(elem1).toggleClass('hidden');
+    $(elem2).toggleClass('hidden');
 }
-function startOver() {
+function nextQuestion(quiz) {
+    j++;
+    if (j < quiz.length) {
+        console.log('next question: ',j);
+        removeContent();
+        if (quiz === htmlQuestions) {
+            createAnswersList(htmlQuestions);
+        } else if ( quiz === jsQuestions) {
+            createAnswersList(jsQuestions);
+        } else {
+            console.log('none');
+        }
+        
+    } else {
+        var initialsEntry = initials.value;
+        var finalScore = score + timeRemaining;
+        removeContent();
+        toggleHidden(answerBtn, finishedBtn);
+        stopTimer();
+        question.textContent = `Congratulations! You finished with a score of ${finalScore}`;
+
+        initials.setAttribute('type', 'text');
+        initials.setAttribute('id', 'initialsEntry');
+        initials.required = true;
+        initialsLabel.setAttribute('class', 'labelOverlay');
+        initialsLabel.setAttribute('for', 'initialsEntry')
+        initialsLabel.textContent = 'Enter your name';
+
+        formDiv.append(initials);
+        formDiv.append(initialsLabel);
+        
+        highScores.push({initialsEntry, finalScore});
+        localStorage.setItem('a', JSON.stringify(highScores));
+        // create new object for scores
+        // then need to JSON.stringify(newObj)
+        // localStorage.setItem('a', JSON.stringify(newObj));
+        // JSON.parse(localStorage.getItem('a));
+        //  OR   JSON.parse(localStorage.a);
+
+        // Can only store strings - cannot append in localStorage
+        // Can only store completed objects, so to append
+        // you need to parse strings back into objects,
+        // append objects, then re-stringify to save anew
+    }
+}
+function createAnswersList(quiz) {
+    question.textContent = quiz[j].title;
+    for (var i = 0; i < quiz[j].choices.length; i++) {
+        var answersLabel = document.createElement('label');
+        var answersRadio = document.createElement('input');
+
+        answersLabel.textContent = quiz[j].choices[i];        
+            
+        answersRadio.setAttribute('type', 'radio');
+        answersRadio.setAttribute('name', 'answer');
+        answersRadio.setAttribute('id', `choice${i}`); // check attribut api to see if they can be combined
+        answersLabel.setAttribute('for', `choice${i}`);
+        formDiv.appendChild(answersRadio);
+        formDiv.appendChild(answersLabel);
+    }
+}
+function quiz(selection) {
+    toggleHidden(jsWrapper, introWrapper);
+    removeContent();
+    startTimer();
+    createAnswersList(selection);
+}
+function compareAnswers() {
+    // check to see if an option is selected when answerBtn is pressed
+    var answersElm = document.querySelector('input:checked + label');
+    if (answersElm) {
+        var answers = answersElm.textContent;
+        if (answers === jsQuestions[j].answer) {
+            score += 10;
+            nextQuestion(jsQuestions);
+        }else if (answers === htmlQuestions[j].answer) {
+            score += 10;
+            nextQuestion(htmlQuestions);
+        } else {
+            penalty();
+        }
+    } else { alert('Please select an answer');}
+}
+
+// Should be possible to just have function quiz and pass which quiz as an argument
+answerBtn.addEventListener('click', event => {
     event.preventDefault();
-    jsWrapper.classList = 'wrapper hidden'; // .add() need help with className vs classList!
-    introWrapper.classList = 'wrapper';
-    // save local storage
-    // check score against high scores
-    // if highscore then would need to enter a separate panel
-    // these comments should probably be a separate function?
-}
-
-function jsQuiz() {
-    startQuiz();
-    removeContent();
-    startTimer();
-    question.textContent = jsQuestions[j].title;
-    for (var i = 0; i < jsQuestions[j].choices.length; i++) {
-        var answersLabel = document.createElement('label');
-        var answersRadio = document.createElement('input');
-        
-        answersRadio.setAttribute('type', 'radio');
-        answersRadio.setAttribute('name', 'answer');
-        answersRadio.setAttribute('id', `choice${i}`); // check attribut api to see if they can be combined
-        answersLabel.textContent = jsQuestions[j].choices[i];
-        answersLabel.setAttribute('for', `choice${i}`);
-
-        formDiv.appendChild(answersRadio);
-        formDiv.appendChild(answersLabel);
-        
-    } // should this event listener be located inside the function? I feel like there's a better way to do this
-    answerBtn.addEventListener('click', event => {
-        event.preventDefault();
-        var answersElm = document.querySelector('input:checked + label');
-        // check to see if an option is selected when answerBtn is pressed
-        if (answersElm) { // research json collections. not necessary here but still look up
-            var answers = answersElm.textContent;
-            if (answers === jsQuestions[j].answer) {
-                score += 10; 
-            }
-            else {
-                penalty();
-            }
-
-            j++;
-
-            if (j === jsQuestions.length) {
-                var finalScore = score + timeRemaining;
-                removeContent();
-                stopTimer();
-                question.textContent = `Congratulations! You finished with a score of ${finalScore}`;
-
-                var initials = document.createElement('input');
-                var initialsLabel = document.createElement('label');
-                var initialsEntry = initials.value;
-
-                initials.setAttribute('type', 'text');
-                initials.setAttribute('id', 'initialsEntry');
-                initials.required = true;
-                initialsLabel.setAttribute('class', 'labelOverlay');
-                initialsLabel.setAttribute('for', 'initialsEntry')
-                initialsLabel.textContent = 'Enter your name';
-    
-                formDiv.append(initials);
-                formDiv.append(initialsLabel);
-
-                localStorage.setItem(initialsEntry, finalScore);
-                console.log(localStorage);
-
-                postScores();
-
-                finishedBtn.classList.remove('hidden');
-                answerBtn.classList.add('hidden');
-                // need to add a start over button that returns you to home page
-            }
-            else {
-                jsQuiz();
-            }
-        }
-    });
-};
-function htmlQuiz() {
-    startQuiz();
-    removeContent();
-    startTimer();
-    question.textContent = htmlQuestions[k].title;
-    for (var i = 0; i < htmlQuestions[k].choices.length; i++) {
-        var answersLabel = document.createElement('label');
-        var answersRadio = document.createElement('input');
-        
-        answersRadio.setAttribute('type', 'radio');
-        answersRadio.setAttribute('name', 'answer');
-        answersRadio.setAttribute('id', `choice${i}`); // check attribut api to see if they can be combined
-        answersLabel.textContent = htmlQuestions[k].choices[i];
-        answersLabel.setAttribute('for', `choice${i}`);
-
-        formDiv.appendChild(answersRadio);
-        formDiv.appendChild(answersLabel);
-        
-    } // should this event listener be located inside the function? I feel like there's a better way to do this
-    answerBtn.addEventListener('click', event => {
-        event.preventDefault();
-        var answersElm = document.querySelector('input:checked + label');
-        // check to see if an option is selected when answerBtn is pressed
-        if (answersElm) { // research json collections. not necessary here but still look up
-            var answers = answersElm.textContent;
-            if (answers === htmlQuestions[k].answer) {
-                score += 10; 
-            }
-            else {
-                penalty();
-            }
-
-            k++;
-
-            if (k === htmlQuestions.length) {
-                var finalScore = score + timeRemaining;
-                removeContent();
-                stopTimer();
-                question.textContent = `Congratulations! You finished with a score of ${finalScore}`;
-
-                var initials = document.createElement('input');
-                var initialsLabel = document.createElement('label');
-                var initialsEntry = initials.value;
-
-                initials.setAttribute('type', 'text');
-                initials.setAttribute('id', 'initialsEntry');
-                initials.required = true;
-                initialsLabel.setAttribute('class', 'labelOverlay');
-                initialsLabel.setAttribute('for', 'initialsEntry')
-                initialsLabel.textContent = 'Enter your name';
-    
-                formDiv.append(initials);
-                formDiv.append(initialsLabel);
-
-                localStorage.setItem(initialsEntry, finalScore);
-                console.log(localStorage); 
-                // create new object for scores
-                // then need to JSON.stringify(newObj)
-                // localStorage.setItem('a', JSON.stringify(newObj));
-                // JSON.parse(localStorage.getItem('a));
-                //  OR   JSON.parse(localStorage.a);
-
-                // Can only store strings - cannot append in localStorage
-                // Can only store completed objects, so to append
-                // you need to parse strings back into objects,
-                // append objects, then re-stringify to save anew
+    compareAnswers();
+});
+startJs.addEventListener('click', function(){quiz(jsQuestions)}); // prevent default?
+startHtml.addEventListener('click', function(){quiz(htmlQuestions)});
+finishedBtn.addEventListener('click', event => {
+    event.preventDefault();
+    toggleHidden(introWrapper, jsWrapper);
+});
 
 
-                postScores();
-
-                finishedBtn.classList.remove('hidden');
-                answerBtn.classList.add('hidden');
-                // need to add a start over button that returns you to home page
-            }
-            else {
-                htmlQuiz();
-            }
-        }
-    });
-};
-
-startJs.addEventListener('click', jsQuiz); // prevent default?
-startHtml.addEventListener('click', htmlQuiz);
-finishedBtn.addEventListener('click', startOver);
 
 
 
